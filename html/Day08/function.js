f301a7bd1bb70679dfaa5045c1689204f55a6c2f
unknown <juboy438@gmail.com>
2022-08-01 16:21:49 +0900

    first commit

---
+++ html/Day08/function.js
@@ -0,0 +1,7 @@
+console.log('---------------------------------');
+console.log('function.js 내부에 실행되는 코드');
+console.log('-------------------------------');
+
+function outerFunc() {
+    console.log('function.js 안에서 선언된 함수');
+}
